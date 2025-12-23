941b29ddd4ae8d7c458251586b39f79ddebf5979
Yaroviy David <david@MacBook-Air-Yaroviy.local>
2025-12-23 21:23:29 +0200

    Update files

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from aiogram import Bot, Dispatcher, Router, types, F
 from aiogram.filters import Command
+import config
+
 import asyncio
 
-bot = Bot('8208985752:AAFQaRe8fqZNXAd_-qLdS0os2TyjXJlNNLM')
+bot = Bot(config.BOT_TOKEN)
 dp = Dispatcher()
 router = Router()
 dp.include_router(router)
+
+@router.message(Command('start'))
+async def start(message: types.Message):
+    await message.answer_invoice(
+        title="Buying the course",
+        description="Buying the David`s course",
+        payload="invoice",
+        provider_token=config.PAYMENT_TOKEN,
+        currency="USD",
+        prices=[
+            types.LabeledPrice(label="Buying the course", amount=5 * 100)
+        ]
+    )
+
+@router.message(F.successful_payment)
+async def success(message: types.Message):
+    await message.answer(f'Success:{message.successful_payment.order_info}')
 
 async def main():
     await dp.start_polling(bot)
